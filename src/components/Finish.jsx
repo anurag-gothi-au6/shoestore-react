fc94fb94fc7bf2a547acdb21a9fa2a525816dbd7
anurag-gothi-au6 <advisoranurag@gmail.com>
2020-07-11 23:34:02 +0530

    responsive

--- src/components/Finish.jsx
+++ src/components/Finish.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-
+import '../styles/Finish.css'
 class Finish extends Component {
   state = {
     total : localStorage.getItem('total')
@@ -11,11 +11,13 @@ class Finish extends Component {
       localStorage.setItem('total',0)
   }
   render() {
-    return this.props.user === null ? (
+    return this.props.user !== null ? (
       <Redirect to="/register" />
     ) : (
-      <div>
+      <div className='finish'>
           <p className='head'>Thank You For Buying!!! Total Purchase: {this.state.total}</p>
+          <div className='break1' />
+          <img className='gif' src="https://media.giphy.com/media/LO4KIaBWEUTyE/giphy.gif" alt="Thank You For Shopping" />
       </div>
     );
   }
